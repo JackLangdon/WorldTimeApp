2ebb842616c7ae5ddc7eee3082082329ff5223bb
John <jlross90@gmail.com>
2020-10-11 13:00:17 +1100

    style loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -29,8 +30,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitWanderingCubes(
+          color: Colors.white,
+          size: 70.0,
+        ),
       ),
     );
   }
